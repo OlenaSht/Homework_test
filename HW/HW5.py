21c43380a1a3a921dc9bd3dfc69c0ae30b4f7d52
OlenaSht <shteplekarelena@gmail.com>
2023-09-20 03:35:51 +0300

    Adding get method for athribute

--- HW/HW5.py
+++ HW/HW5.py
@@ -10,9 +10,14 @@ class Animals:
     def calculation(self):
         return f'Next year you will be {self.age + 1} year old'
 
+    def set_name(self, new_name):
+        self.name = new_name
+
+
 
 animal1 = Animals('Rex', 5, 'husky')
 animal2 = Animals(type1 = 'JackRusselTerrier', age = 2, name = 'Ted')
+print(animal2.set_name('Theodor')) #it was set/get
 print(animal1.__dict__)
 print(animal2.__dict__)
 print(animal1.name)
@@ -22,8 +27,8 @@ print(animal1.calculation())
 class Doggos(Animals):
     def __init__(self, name, age, type1, fur_color, eye_color):
         super().__init__(name, age, type1)
-        self.fur_color = fur_color
-        self.eye_color = eye_color
+        self.__fur_color = fur_color
+        self._eye_color = eye_color
 
     def confession(self):
         return 'I love doggos!'
@@ -31,5 +36,7 @@ class Doggos(Animals):
 dog1 = Doggos('Larry', 1, 'rotweiler', 'brown', 'blue')
 dog2 = Doggos('Terry', 6, 'labrador', 'grey', 'brown')
 print(dog2.__dict__)
+#print(dog2.fur_color) '''скрытое,'Doggos' object has no attribute 'fur_color', privete'''
+print(dog1._eye_color) #protected
 print(dog2.calculation())
 print(dog2.confession())
